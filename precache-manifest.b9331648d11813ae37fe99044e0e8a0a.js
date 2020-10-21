d453dda73be309c1593875efac6b4bba70817b55
himanshu-kumar-steadybiz <himanshu.kumar@steadybiz.in>
2020-10-21 13:48:16 -0700

    Updates

---
+++ precache-manifest.b9331648d11813ae37fe99044e0e8a0a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "596fc572931d64ab0bfd3aba105e397e",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "3d39f663c1b3dae3c9f5",
+    "url": "/test/static/css/2.588f4407.chunk.css"
+  },
+  {
+    "revision": "190e3cec4edc2173e7ca",
+    "url": "/test/static/css/main.41e6fce8.chunk.css"
+  },
+  {
+    "revision": "3d39f663c1b3dae3c9f5",
+    "url": "/test/static/js/2.72ca8ab2.chunk.js"
+  },
+  {
+    "revision": "190e3cec4edc2173e7ca",
+    "url": "/test/static/js/main.7a17c9ff.chunk.js"
+  },
+  {
+    "revision": "2160d828785e96a67ccd",
+    "url": "/test/static/js/runtime~main.eded8796.js"
+  },
+  {
+    "revision": "1fbfd4bcffccb94e8e8a5ea70616b296",
+    "url": "/test/static/media/lg.1fbfd4bc.woff"
+  },
+  {
+    "revision": "2ec2cb2199d4d881e6a6ad86690f6add",
+    "url": "/test/static/media/lg.2ec2cb21.svg"
+  },
+  {
+    "revision": "f4292655f93dd12d9b8e4fc067ef2489",
+    "url": "/test/static/media/lg.f4292655.ttf"
+  }
+]);
